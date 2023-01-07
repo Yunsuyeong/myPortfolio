3ff6c9ec313723d5ff8eb7f3c1c2dcb5faa3d544
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-01-08 00:02:54 +0900

    2023/01/08(Home Page Animation)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/header";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Home: NextPage = () => {
   const [hidden, setHidden] = useState(true);
@@ -9,11 +10,11 @@ const Home: NextPage = () => {
   return (
     <div>
       {hidden ? (
-        <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
+        <div className="w-screen h-screen flex flex-col gap-6 justify-center items-center white">
           <svg
             onClick={toggle}
             className="w-12 h-12 cursor-pointer"
-            fill="white"
+            fill="black"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -22,20 +23,29 @@ const Home: NextPage = () => {
             001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
             ></path>
           </svg>
+          <h1 className="text-xl text-black font-bold">홈으로 이동</h1>
         </div>
       ) : (
-        <div className="w-screen h-screen flex flex-col gap-10 justify-center items-center bg-blue-200">
-          <Head>
-            <title>YSY's site</title>
-          </Head>
-          <Header />
-          <div className="flex flex-col gap-10 justify-center items-center">
-            <h1 className="text-black text-3xl font-extrabold">Hello!</h1>
-            <span className="text-gray-900 text-2xl font-bold">
-              This is My personal portfolio site!
-            </span>
-          </div>
-        </div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ type: "keyframes", duration: 1.5 }}
+            className="w-screen h-screen flex flex-col gap-10 justify-center items-center bg-blue-200"
+          >
+            <Head>
+              <title>YSY's site</title>
+            </Head>
+            <Header />
+            <div className="flex flex-col gap-10 justify-center items-center">
+              <h1 className="text-black text-3xl font-extrabold">Hello!</h1>
+              <span className="text-gray-900 text-2xl font-bold">
+                This is My personal portfolio site!
+              </span>
+            </div>
+          </motion.div>
+        </AnimatePresence>
       )}
     </div>
   );
