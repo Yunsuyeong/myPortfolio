22b2bd52d1adfa28b41a0a888c5d67ff561f988b
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-01-14 16:31:28 +0900

    2023/01/14(Edit Navbar)

--- components/header.tsx
+++ components/header.tsx
@@ -1,41 +1,81 @@
 import Link from "next/link";
+import styled from "styled-components";
 import { useRouter } from "next/router";
-import { cls } from "../libs/utils";
+import { motion, useScroll, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+
+const Circle = styled(motion.span)`
+  position: absolute;
+  bottom: -10px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: white;
+`;
+
+const navVariants = {
+  up: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  },
+};
 
 export default function Header() {
   const router = useRouter();
-  const onClick = () => {
-    router.back();
-  };
+  console.log(router);
+  const { scrollY } = useScroll();
+  const navAnimation = useAnimation();
+  useEffect(() => {
+    scrollY.onChange(() => {
+      if (scrollY.get() > 60) {
+        navAnimation.start("scroll");
+      } else {
+        navAnimation.start("up");
+      }
+    });
+  }, [scrollY, navAnimation]);
   return (
-    <div>
-      <nav className="fixed top-5 left-10 text-2xl font-bold text-gray-700 hover:text-gray-900">
+    <motion.nav
+      variants={navVariants}
+      initial="up"
+      animate={navAnimation}
+      className="fixed top-0 w-screen h-[60px] flex justify-between items-center px-12"
+    >
+      <div className="text-xl sm:text-2xl font-bold text-gray-500 hover:text-white">
         <Link legacyBehavior href="/">
-          <a className="flex flex-col items-center space-y-2">
+          <a className="flex flex-col items-center">
             <span>YSY's site</span>
           </a>
         </Link>
-      </nav>
-      <nav
-        className="fixed top-5 right-5 max-w-lg flex justify-around items-end w-full py-2 text-xl font-bold
-       text-gray-500"
-      >
+      </div>
+      <div className="w-full max-w-sm flex justify-around text-lg sm:text-xl font-bold text-gray-500">
         <Link legacyBehavior href="/about">
           <a className="flex flex-col items-center space-y-2 hover:text-white">
-            <span>About</span>
+            <span className="relative flex flex-col justify-center">
+              About {router.route == "/about" && <Circle layoutId="circle" />}
+            </span>
           </a>
         </Link>
         <Link legacyBehavior href="/study">
-          <a className="flex flex-col items-center space-y-2 hover:text-white">
-            <span>Study</span>
+          <a className="relative flex flex-col items-center space-y-2 hover:text-white">
+            <span className="relative flex flex-col justify-center">
+              Study {router.route == "/study" && <Circle layoutId="circle" />}
+            </span>
           </a>
         </Link>
         <Link legacyBehavior href="/work">
-          <a className="flex flex-col items-center space-y-2 hover:text-white">
-            <span>Work</span>
+          <a className="relative flex flex-col items-center space-y-2 hover:text-white">
+            <span className="relative flex flex-col justify-center">
+              Work {router.route == "/work" && <Circle layoutId="circle" />}
+            </span>
           </a>
         </Link>
-      </nav>
-    </div>
+      </div>
+    </motion.nav>
   );
 }
