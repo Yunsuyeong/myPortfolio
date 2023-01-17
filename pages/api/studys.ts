f63603d5211bfafd6dc98ad7b7e75b63c3ca82f5
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-01-17 23:58:33 +0900

    2023/01/17(Study Page)

---
+++ pages/api/studys.ts
@@ -0,0 +1,16 @@
+export const Studys = [
+  {
+    id: 1,
+    title: "Netflix Clone",
+    src: "",
+    link: "",
+    description: "넷플릭스 홈페이지를 따라 만들어본 클론코딩 사이트입니다.",
+  },
+  {
+    id: 2,
+    title: "XXXXXXXXXX",
+    src: "",
+    link: "",
+    description: "xxxxxxxxxx",
+  },
+];
--- pages/study/index.tsx
+++ pages/study/index.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../../components/header";
+import { Studys } from "../api/studys";
 
 const Study: NextPage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col gap-10 justify-start items-center bg-blue-200">
+    <div className="w-screen h-full flex flex-col gap-10 justify-start items-center bg-blue-200">
       <Head>
         <title>YSY's Study</title>
       </Head>
@@ -27,16 +29,28 @@ const Study: NextPage = () => {
         </svg>
         <h1 className="text-black text-[36px] font-extrabold">STUDY</h1>
       </div>
-      <div className="flex flex-col items-center pt-12">
-        <span className="text-gray-900 text-2xl font-bold">
-          My name is Suyeong Yun. <br />
-          <br />
-          I am attending Incheon National University.
-          <br />
-          <br />
-          I'm interested in Web programming. (frontend, backend)
-        </span>
-      </div>
+      {Studys.map((study) => (
+        <div
+          key={study.id}
+          className="w-2/3 flex flex-col justify-center gap-5"
+        >
+          <div className="flex flex-col justify-center gap-2 border-white border-2 px-6 py-2">
+            <h2 className="font-bold text-[28px] text-center">
+              {study.id}. {study.title}
+            </h2>
+            <img src={study.src} className="w-72 h-72 pt-4" />
+            <div className="text-[20px]">
+              <span>GITHUB : </span>
+              <Link legacyBehavior href={study.link}>
+                <a target="_blank" className="font-semibold">
+                  <span>{study.link}</span>
+                </a>
+              </Link>
+            </div>
+            <p className="text-[16px]">{study.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
