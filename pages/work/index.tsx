aceeac6ec7df8070180222d056cceeeb4dc02f15
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-01-24 00:14:11 +0900

    2023/01/24(Edit Work Page)

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -10,11 +10,12 @@ import styled from "styled-components";
 const offset = 1;
 
 const WorkBox = styled(motion.div)`
-  width: 60vw;
+  width: 50vw;
   display: flex;
   flex-direction: column;
+  margin: auto;
   padding: 24px 8px;
-  gap: 20px;
+  gap: 10px;
   border-width: 2px;
 `;
 
@@ -56,60 +57,90 @@ const Work: NextPage = () => {
         <title>YSY's Work</title>
       </Head>
       <Header />
-      <div
-        className="w-screen h-screen flex flex-col gap-6 items-center bg-black"
-        onClick={increaseIndex}
-      >
-        <div className="pt-12 flex items-center gap-6">
-          <svg
-            className="w-12 h-12"
-            fill="white"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414
+      <div className="flex items-center bg-black">
+        <svg
+          fill="white"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          className="fixed left-12 h-8 w-8 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+          ></path>
+        </svg>
+        <div className="w-full h-screen flex flex-col items-center gap-6">
+          <div className="pt-12 flex items-center gap-6">
+            <svg
+              className="w-12 h-12"
+              fill="white"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414
         1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828
         0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2
         2 0 11-2.828-2.828l3-3z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <h1 className="text-white text-[36px] font-extrabold">WORK</h1>
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <h1 className="text-white text-[36px] font-extrabold">WORK</h1>
+          </div>
+          <div className="relative top-[20px] right-1/4 text-white">
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <motion.div
+                key={index}
+                initial={{ x: Window * 2 }}
+                animate={{ x: 0 }}
+                exit={{ x: -Window * 2 }}
+                transition={{ type: "tween", duration: 1 }}
+                className="absolute w-full grid grid-cols-6"
+              >
+                {Works.slice(offset * index, offset * index + offset).map(
+                  (work) => (
+                    <WorkBox key={work.id}>
+                      <h2 className="font-bold text-[28px] text-center">
+                        {work.id}. {work.title}
+                      </h2>
+                      <img src={work.src} className="w-72 h-72 pt-4" />
+                      <div className="text-[20px]">
+                        <span>GITHUB : </span>
+                        <Link legacyBehavior href={work.link}>
+                          <a target="_blank" className="font-semibold">
+                            <span>{work.link}</span>
+                          </a>
+                        </Link>
+                      </div>
+                      <p className="text-[16px]">{work.description}</p>
+                    </WorkBox>
+                  )
+                )}
+              </motion.div>
+            </AnimatePresence>
+          </div>
         </div>
-        <div className="relative top-[20px] text-white">
-          <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
-            <motion.div
-              key={index}
-              initial={{ x: Window + 10 }}
-              animate={{ x: -300 }}
-              exit={{ x: -Window - 10 }}
-              transition={{ type: "tween", duration: 1 }}
-              className="absolute w-full grid grid-cols-6"
-            >
-              {Works.slice(offset * index, offset * index + offset).map(
-                (work) => (
-                  <WorkBox key={work.id}>
-                    <h2 className="font-bold text-[28px] text-center">
-                      {work.id}. {work.title}
-                    </h2>
-                    <img src={work.src} className="w-72 h-72 pt-4" />
-                    <div className="text-[20px]">
-                      <span>GITHUB : </span>
-                      <Link legacyBehavior href={work.link}>
-                        <a target="_blank" className="font-semibold">
-                          <span>{work.link}</span>
-                        </a>
-                      </Link>
-                    </div>
-                    <p className="text-[16px]">{work.description}</p>
-                  </WorkBox>
-                )
-              )}
-            </motion.div>
-          </AnimatePresence>
-        </div>
+        <svg
+          fill="white"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          className="fixed right-12 h-8 w-8 cursor-pointer"
+          onClick={increaseIndex}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+          ></path>
+        </svg>
       </div>
     </>
   );
